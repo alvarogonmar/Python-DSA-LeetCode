18d725355d56d94e4b148a690e7a694d2a662475
alvarogonmar <alvarogonmar0@gmail.com>
2025-06-25 21:22:17 -0600

    Add comment to clarify loop iteration in print_items function

--- BigO/DifferentTerms.py
+++ BigO/DifferentTerms.py
@@ -2,7 +2,7 @@ def print_items(a,b): # O(a + b)
     for i in range(a): # for loop runs a times
         print(i) # printing takes O(1) time
 
-    for j in range(b):
+    for j in range(b):  # for loop runs b times
         print(j)
 
 print_items(1, 10)
