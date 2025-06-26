1a93d24cc156063779f9ef8ad7d7a16f617973c8
alvarogonmar <alvarogonmar0@gmail.com>
2025-06-25 21:22:07 -0600

    Add comment to clarify time complexity of print statement in print_items function

--- BigO/DifferentTerms.py
+++ BigO/DifferentTerms.py
@@ -1,6 +1,6 @@
 def print_items(a,b): # O(a + b)
     for i in range(a): # for loop runs a times
-        print(i)
+        print(i) # printing takes O(1) time
 
     for j in range(b):
         print(j)
