ea8369a0d5dbaf17bffd0df58d4b7d0e0229a033
alvarogonmar <alvarogonmar0@gmail.com>
2025-06-26 21:42:42 -0600

    Add comment to clarify the print statement for cookie_two's color

--- Classes_Pointers/Cookie.py
+++ Classes_Pointers/Cookie.py
@@ -18,4 +18,4 @@ print('Cookie two is', cookie_two.get_color()) # Using the get_color method to p
 cookie_one.set_color('yellow') # Changing the color of cookie_one to 'yellow' using the set_color method
 
 print('\nCookie one is now', cookie_one.get_color()) # Printing the new color of cookie_one after changing it
-print('Cookie two is still', cookie_two.get_color())
+print('Cookie two is still', cookie_two.get_color()) # Printing the color of cookie_two to show it remains unchanged
