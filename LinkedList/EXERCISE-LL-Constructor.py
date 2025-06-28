6339c856cd3d281f0e288643556774360d54ffed
alvarogonmar <alvarogonmar0@gmail.com>
2025-06-27 21:08:02 -0600

    Implement LinkedList constructor to initialize head, tail, and length

--- LinkedList/EXERCISE-LL-Constructor.py
+++ LinkedList/EXERCISE-LL-Constructor.py
@@ -20,7 +20,10 @@ class Node:
         
 class LinkedList:
     def __init__(self, value):
-        pass
+        new_node = Node(value);
+        self.head = new_node;
+        self.tail = new_node;
+        self.length = 1;
 
 
  
