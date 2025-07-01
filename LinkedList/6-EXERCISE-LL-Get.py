fbd79ed9eba1b3f823f61f9f6d9623e257254375
alvarogonmar <alvarogonmar0@gmail.com>
2025-06-30 22:04:46 -0600

    Remove incomplete implementation of get method in LinkedList class

--- LinkedList/6-EXERCISE-LL-Get.py
+++ LinkedList/6-EXERCISE-LL-Get.py
@@ -67,12 +67,7 @@ class LinkedList:
         return temp
 
     def get(self, index):
-        if index < 0 or index > self.length:
-            return None
-        temp = self.head
-        for _ in range(index):
-            temp = temp,next
-        return temp
+
         
 
 
