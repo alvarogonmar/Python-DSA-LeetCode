9886eb8ae1605318859ddc08570eacfba6c90950
alvarogonmar <alvarogonmar0@gmail.com>
2025-06-30 22:05:50 -0600

    Fix get method in LinkedList class to correctly traverse nodes by index

--- LinkedList/6-EXERCISE-LL-Get.py
+++ LinkedList/6-EXERCISE-LL-Get.py
@@ -70,6 +70,8 @@ class LinkedList:
         if index < 0 or index > self.length:
             return None
         temp = self.head
+        for _ in range(index):
+            temp = temp,next
 
         
 
