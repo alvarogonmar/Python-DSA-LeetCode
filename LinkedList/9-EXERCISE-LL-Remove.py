b09b4ed15c86479057c0285d6ffed8538ebb9e43
alvarogonmar <alvarogonmar0@gmail.com>
2025-07-02 22:27:59 -0600

    Remove unused remove method from LinkedList class

--- LinkedList/9-EXERCISE-LL-Remove.py
+++ LinkedList/9-EXERCISE-LL-Remove.py
@@ -95,19 +95,7 @@ class LinkedList:
         self.length += 1   
         return True  
 
-    def remove(self, index):
-        if index < 0 or index >= self.length:
-            return None
-        if index == 0:
-            return self.pop_first()
-        if index == self.length:
-            return self.pop()
-        pre = self.get(index - 1)
-        temp = pre.next
-        pre.next = temp.next
-        temp.next = None
-        self.length -= 1
-        return temp
+
 
 
   
