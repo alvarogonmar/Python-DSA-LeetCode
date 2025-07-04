c1ca36b1f70fb4c889aba06f4d1fef4e9c2b7535
alvarogonmar <alvarogonmar0@gmail.com>
2025-07-04 17:43:00 -0600

    Add constructor for DoublyLinkedList to initialize with a new node

--- DoublyLinkedList/EXERCISE-DLL-Constructor.py
+++ DoublyLinkedList/EXERCISE-DLL-Constructor.py
@@ -20,6 +20,10 @@ class Node:
         self.next = None
         self.prev = None
 
+class DoublyLinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+
 
 my_doubly_linked_list = DoublyLinkedList(7)
 
