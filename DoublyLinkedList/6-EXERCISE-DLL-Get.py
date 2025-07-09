8649a7546e47d01a603772287d919c7d9da3ded4
alvarogonmar <alvarogonmar0@gmail.com>
2025-07-08 21:42:49 -0600

    Remove redundant implementation of get method in DoublyLinkedList

--- DoublyLinkedList/6-EXERCISE-DLL-Get.py
+++ DoublyLinkedList/6-EXERCISE-DLL-Get.py
@@ -72,17 +72,6 @@ class DoublyLinkedList:
         return temp
 
     def get(self, index):
-        if index < 0 or index >= self.length:
-            return None
-        temp = self.head
-        if index < self.length/2:
-            for _ in range(self.length):
-                temp = temp.next
-        else:
-            temp = self.tail
-            for _ in range(self.length - 1, index, -1):
-                temp = temp.prev
-        return temp
 
 
 
