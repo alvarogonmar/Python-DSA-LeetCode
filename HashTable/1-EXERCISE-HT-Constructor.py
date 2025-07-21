494e05940e8e43190f2d0c4747d0ced252ba5227
alvarogonmar <alvarogonmar0@gmail.com>
2025-07-20 18:22:53 -0600

    Add initial implementation of HashTable class with expected output

---
+++ HashTable/1-EXERCISE-HT-Constructor.py
@@ -0,0 +1,21 @@
+class HashTable:
+
+
+        
+my_hash_table = HashTable()
+
+my_hash_table.print_table()
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    0 :  None
+    1 :  None
+    2 :  None
+    3 :  None
+    4 :  None
+    5 :  None
+    6 :  None
+
+"""
