19174494bb4dec93608bb7ffd9edcde95cc231f9
alvarogonmar <alvarogonmar0@gmail.com>
2025-07-23 20:42:59 -0600

    Add loop to iterate over items in get_item method of HashTable class

--- HashTable/3-EXERCISE-HT-Get.py
+++ HashTable/3-EXERCISE-HT-Get.py
@@ -21,6 +21,7 @@ class HashTable:
     def get_item(self, key):
         index = self.__hash(key)
         if self.data_map[index] is not None:
+            for i in range(len(self.data_map[index])):
 
 
         
