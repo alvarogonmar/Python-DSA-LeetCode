b5053de8255a93472141f1be399650e8d063b2f9
alvarogonmar <alvarogonmar0@gmail.com>
2025-07-23 20:43:25 -0600

    Fix return statement in get_item method of HashTable class to ensure correct value retrieval

--- HashTable/3-EXERCISE-HT-Get.py
+++ HashTable/3-EXERCISE-HT-Get.py
@@ -23,6 +23,7 @@ class HashTable:
         if self.data_map[index] is not None:
             for i in range(len(self.data_map[index])):
                 if self.data_map[index][i][0] == key:
+                    return self.data_map[index][i][1]
 
 
         
