edfbbbf3670b3ad905eba3e1c639ee32bfc05f73
alvarogonmar <alvarogonmar0@gmail.com>
2025-07-25 20:45:55 -0600

    Fix keys method in HashTable class to append keys correctly

--- HashTable/4-EXERCISE-HT-Keys.py
+++ HashTable/4-EXERCISE-HT-Keys.py
@@ -31,6 +31,7 @@ class HashTable:
         for i in range(len(self.data_map)):
             if self.data_map[i] is not None:
                 for j in range(len(self.data_map[i])):
+                    all_keys.append(self.data_map[i][j][0])
 
         
 
