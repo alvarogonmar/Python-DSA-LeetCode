089df8eda1d83e796773a9e3d1c7d3b04e001d7e
alvarogonmar <alvarogonmar0@gmail.com>
2025-07-28 18:17:24 -0600

    Fix add_edge method to ensure it returns a boolean value after adding an edge

--- Graphs/2-EXERCISE-GR-Add_Edge.py
+++ Graphs/2-EXERCISE-GR-Add_Edge.py
@@ -16,6 +16,7 @@ class Graph:
         if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
             self.adj_list[v1].append(v2)
             self.adj_list[v2].append(v1)
+            return True
 
 
 
