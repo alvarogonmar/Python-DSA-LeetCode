b94725a028035a12954c688b64869724e275f743
alvarogonmar <alvarogonmar0@gmail.com>
2025-07-28 19:11:12 -0600

    Implement edge removal in remove_edge method

--- Graphs/3-EXERCISE-GR-Remove_Edge.py
+++ Graphs/3-EXERCISE-GR-Remove_Edge.py
@@ -22,6 +22,7 @@ class Graph:
     def remove_edge(self, v1, v2):
         if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
             try:
+                self.adj_list[v1].remove(v2)
 
 
 
