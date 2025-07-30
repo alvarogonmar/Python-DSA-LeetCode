1a76c4add84c6ca9f4115b3e298907a871613a4c
alvarogonmar <alvarogonmar0@gmail.com>
2025-07-29 22:26:01 -0600

    Fix remove_vertex method to properly remove associated edges from the graph

--- Graphs/4-EXERCISE-GR-Remove_Vertex.py
+++ Graphs/4-EXERCISE-GR-Remove_Vertex.py
@@ -32,6 +32,7 @@ class Graph:
     def remove_vertex(self, vertex):
         if vertex in self.adj_list.keys():
             for other_vertex in self.adj_list[vertex]:
+                self.adj_list[other_vertex].remove(vertex)
 
 
 
