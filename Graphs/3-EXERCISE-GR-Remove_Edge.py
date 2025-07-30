e4d8d488503d0704cd1a12285786f98accfa6bbb
alvarogonmar <alvarogonmar0@gmail.com>
2025-07-29 21:44:15 -0600

    Fix remove_edge method to ensure both vertices are removed from each other's adjacency list

--- Graphs/3-EXERCISE-GR-Remove_Edge.py
+++ Graphs/3-EXERCISE-GR-Remove_Edge.py
@@ -23,8 +23,7 @@ class Graph:
         if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
             try:
                 self.adj_list[v1].remove(v2)
-
-
+                self.adj_list[v2].remove(v1)
 
 
 
