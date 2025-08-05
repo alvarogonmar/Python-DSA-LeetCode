48645ce5a11292c3fc60b1bef2313e1697869ee5
alvarogonmar <alvarogonmar0@gmail.com>
2025-08-04 21:58:07 -0600

    Add comment to funcTwo to clarify call to funcThree

--- Recursion/1-CallStack.py
+++ Recursion/1-CallStack.py
@@ -2,7 +2,7 @@ def funcThree(): # Function that calls itself
     print('Three') # Base case is not needed here, but it could be added for a real recursive function
 
 def funcTwo(): # Function that calls another function
-    funcThree()
+    funcThree() # Call to funcThree, which will print 'Three'
     print('Two')
 
 def funcOne():
