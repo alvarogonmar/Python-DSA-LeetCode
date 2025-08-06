418e7c66d2a5cd22b5a802a8a95db23093be0bb6
alvarogonmar <alvarogonmar0@gmail.com>
2025-08-05 21:35:14 -0600

    Add comment to clarify output of factorial function call

--- Recursion/2-Factorial.py
+++ Recursion/2-Factorial.py
@@ -4,5 +4,5 @@ def factorial(n): # Function to calculate factorial of n
     return n * factorial(n-1) # Recursive call to calculate factorial of n-1
 
 
-print(factorial(4))
+print(factorial(4)) # This will print the factorial of 4, which is 24
 
