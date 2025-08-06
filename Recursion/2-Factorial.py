5b007f0498baf9f8002e0be5fae695905a8f0e45
alvarogonmar <alvarogonmar0@gmail.com>
2025-08-05 21:35:06 -0600

    Add comment to factorial function to clarify recursive call

--- Recursion/2-Factorial.py
+++ Recursion/2-Factorial.py
@@ -1,7 +1,7 @@
 def factorial(n): # Function to calculate factorial of n
     if n == 1: # Base case for recursion
         return 1 # If n is 1, return 1
-    return n * factorial(n-1)
+    return n * factorial(n-1) # Recursive call to calculate factorial of n-1
 
 
 print(factorial(4))
