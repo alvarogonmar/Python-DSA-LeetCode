b822a69433898e645ce040ff6af8b456bff5bb2a
alvarogonmar <alvarogonmar0@gmail.com>
2025-08-05 21:34:59 -0600

    Add comment to factorial function to clarify return value for base case

--- Recursion/2-Factorial.py
+++ Recursion/2-Factorial.py
@@ -1,6 +1,6 @@
 def factorial(n): # Function to calculate factorial of n
     if n == 1: # Base case for recursion
-        return 1
+        return 1 # If n is 1, return 1
     return n * factorial(n-1)
 
 
