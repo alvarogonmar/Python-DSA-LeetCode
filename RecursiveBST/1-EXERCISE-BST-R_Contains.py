797135de03d85cb528ca45a236f6a4bf1417327a
alvarogonmar <alvarogonmar0@gmail.com>
2025-08-06 20:44:21 -0600

    Add return statement for found value in contains method of BinarySearchTree class

--- RecursiveBST/1-EXERCISE-BST-R_Contains.py
+++ RecursiveBST/1-EXERCISE-BST-R_Contains.py
@@ -37,6 +37,7 @@ class BinarySearchTree:
             elif value > temp.value:
                 temp = temp.right
             else:
+                return True
 
         
     ## WRITE R_CONTAINS METHODS HERE ##
