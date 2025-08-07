50b321a019321c15ce16fcd17e173f65474afad0
alvarogonmar <alvarogonmar0@gmail.com>
2025-08-06 20:43:54 -0600

    Add condition to handle greater value in contains method of BinarySearchTree class

--- RecursiveBST/1-EXERCISE-BST-R_Contains.py
+++ RecursiveBST/1-EXERCISE-BST-R_Contains.py
@@ -34,6 +34,7 @@ class BinarySearchTree:
         while (temp is not None):
             if value < temp.value:
                 temp = temp.left
+            elif value > temp.value:
 
         
     ## WRITE R_CONTAINS METHODS HERE ##
