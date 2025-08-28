6f0863ebd09a19fe879d5823257e61b38d1639ff
alvarogonmar <alvarogonmar0@gmail.com>
2025-08-28 09:42:53 -0600

    Add temporary variable for swapping elements in bubble_sort function

--- BasicSorts/1-EXERCISE-Bubble_Sort.py
+++ BasicSorts/1-EXERCISE-Bubble_Sort.py
@@ -10,6 +10,7 @@ def bubble_sort(my_list):
     for i in range(len(my_list) - 1, 0, -1):
         for j in range(i):
             if my_list[j] > my_list[j+1]:
+                temp = my_list[j]
 
 
 
