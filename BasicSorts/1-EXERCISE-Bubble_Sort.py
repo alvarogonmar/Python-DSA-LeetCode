d61cb339d624e0a78b34be35f16ea0e67e9084ac
alvarogonmar <alvarogonmar0@gmail.com>
2025-08-28 23:19:29 -0600

    Add comment to bubble_sort function for clarity

--- BasicSorts/1-EXERCISE-Bubble_Sort.py
+++ BasicSorts/1-EXERCISE-Bubble_Sort.py
@@ -6,7 +6,7 @@
 ##################################### 
     
 
-def bubble_sort(my_list):
+def bubble_sort(my_list): # def inicializadora de bubblesort
     for i in range(len(my_list) - 1, 0, -1):
         for j in range(i):
             if my_list[j] > my_list[j+1]:
