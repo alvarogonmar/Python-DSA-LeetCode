1c18e791f9fe64ab6b8862b1fca213b6fe8d35fa
alvarogonmar <alvarogonmar0@gmail.com>
2025-09-15 18:33:52 -0600

    Add clarification comment for inner loop in selection_sort function

--- BasicSorts/2-EXERCISE-Selection_Sort.py
+++ BasicSorts/2-EXERCISE-Selection_Sort.py
@@ -8,7 +8,7 @@
 def selection_sort(my_list): # selection sort function
     for i in range(len(my_list) - 1): # for i in range from 0 to the second last element
         min_index = i # assume the minimum is the first element
-        for j in range(i+1, len(my_list)):
+        for j in range(i+1, len(my_list)): # for j in range from i+1 to the end of the list
             if my_list[j] < my_list[min_index]:
                 min_index = j
         if min_index != i:
