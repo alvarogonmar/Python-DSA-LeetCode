1ed4c20f9a47700e2409531cf756b08aa5148687
alvarogonmar <alvarogonmar0@gmail.com>
2025-09-17 10:28:36 -0600

    Fix indentation in merge function to ensure proper increment of index j

--- MergeSort/1-EXERCISE-Merge.py
+++ MergeSort/1-EXERCISE-Merge.py
@@ -16,7 +16,8 @@ def merge(list1, list2): # Merge function that merges two sorted lists
             i += 1
         else:
             combined.append(list2[j])
-
+            j += 1
+    
 
 # MERGE REQUIRES TWO SORTED LISTS:
 print(merge([1,2,7,8], [3,4,5,6]))
