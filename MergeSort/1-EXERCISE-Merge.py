3b7dc06b002d61f55f39652436110fc6a67b61c0
alvarogonmar <alvarogonmar0@gmail.com>
2025-09-17 20:44:02 -0600

    Add logic to append remaining elements from list2 in merge function

--- MergeSort/1-EXERCISE-Merge.py
+++ MergeSort/1-EXERCISE-Merge.py
@@ -21,6 +21,7 @@ def merge(list1, list2): # Merge function that merges two sorted lists
     while i < len(list1):
         combined.append(list1[i])
         i += 1
+    while j < len(list2):
 
 
 # MERGE REQUIRES TWO SORTED LISTS:
