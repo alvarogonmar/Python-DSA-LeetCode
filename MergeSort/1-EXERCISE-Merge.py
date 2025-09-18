af58243f3e122a2dc17bb29a9580a04c5102ce6a
alvarogonmar <alvarogonmar0@gmail.com>
2025-09-18 10:57:41 -0600

    Fix merge function to return combined list after merging

--- MergeSort/1-EXERCISE-Merge.py
+++ MergeSort/1-EXERCISE-Merge.py
@@ -25,6 +25,7 @@ def merge(list1, list2): # Merge function that merges two sorted lists
         combined.append(list2[j])
         j += 1
 
+    return combined
 
 # MERGE REQUIRES TWO SORTED LISTS:
 print(merge([1,2,7,8], [3,4,5,6]))
