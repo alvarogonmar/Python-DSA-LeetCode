8ebe872918b085b52ebf97204fbeb07849e84374
alvarogonmar <alvarogonmar0@gmail.com>
2025-09-18 20:04:40 -0600

    Add initial merge function and setup for merge sort implementation

---
+++ MergeSort/2-EXERCISE-Merge_Sort.py
@@ -0,0 +1,30 @@
+def merge(array1, array2):
+    combined = []
+
+## WRITE MERGE_SORT FUNCTION HERE ##
+#                                  #
+#                                  #
+#                                  #
+#                                  #
+####################################
+
+
+
+original_list = [3,1,4,2]
+
+sorted_list = merge_sort(original_list)
+
+print('Original List:', original_list)
+
+print('\nSorted List:', sorted_list)
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    Original List: [3, 1, 4, 2]
+
+    Sorted List: [1, 2, 3, 4]
+
+ """
