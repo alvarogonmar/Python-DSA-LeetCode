60f351709025897aa9872af513ca07c3416ae4c5
alvarogonmar <alvarogonmar0@gmail.com>
2025-09-21 13:58:29 -0600

    Increment swap_index in pivot function for correct partitioning logic

--- QuickSort/2-EXERCISE-Quick_Sort.py
+++ QuickSort/2-EXERCISE-Quick_Sort.py
@@ -9,6 +9,7 @@ def pivot(my_list, pivot_index, end_index):
 
     for i in range(pivot_index+1, end_index+1):
         if my_list[i] < my_list[pivot_index]:
+            swap_index += 1
 
 
 def quick_sort(my_list):
