3ee4778549a8408bcd8c2a76fb06bd04ec8f805f
alvarogonmar <alvarogonmar0@gmail.com>
2025-09-21 13:56:00 -0600

    Fix increment of swap_index in pivot function for correct element sorting

--- QuickSort/1-EXERCISE-Pivot.py
+++ QuickSort/1-EXERCISE-Pivot.py
@@ -15,6 +15,8 @@ def pivot(my_list, pivot_index, end_index):
     swap_index = pivot_index
     for i in range(pivot_index+1, end_index+1):
         if my_list[i] < my_list[pivot_index]:
+            swap_index += 1
+
 
 
 my_list = [4,6,1,7,3,2,5]
