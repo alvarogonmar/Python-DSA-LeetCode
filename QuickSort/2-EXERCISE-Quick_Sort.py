0c0a326dd055fda02ab67ad5b7b2ddaeca16901e
alvarogonmar <alvarogonmar0@gmail.com>
2025-09-21 13:58:14 -0600

    Add loop in pivot function to iterate through elements for partitioning

--- QuickSort/2-EXERCISE-Quick_Sort.py
+++ QuickSort/2-EXERCISE-Quick_Sort.py
@@ -7,6 +7,8 @@ def swap(my_list, index1, index2):
 def pivot(my_list, pivot_index, end_index):
     swap_index = pivot_index
 
+    for i in range(pivot_index+1, end_index+1):
+
 
 def quick_sort(my_list):
     quick_sort_helper(my_list, 0, len(my_list)-1)
