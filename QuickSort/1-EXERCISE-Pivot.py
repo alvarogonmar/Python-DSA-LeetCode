7ef74ba5da035a719db99f15fb770875140d1cad
alvarogonmar <alvarogonmar0@gmail.com>
2025-09-21 13:56:10 -0600

    Fix swap logic in pivot function to ensure correct element positioning

--- QuickSort/1-EXERCISE-Pivot.py
+++ QuickSort/1-EXERCISE-Pivot.py
@@ -16,6 +16,7 @@ def pivot(my_list, pivot_index, end_index):
     for i in range(pivot_index+1, end_index+1):
         if my_list[i] < my_list[pivot_index]:
             swap_index += 1
+            swap(my_list, swap_index, i)
 
 
 
