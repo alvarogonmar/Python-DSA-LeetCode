c6d4bfc7ed22ffc2134fd90f45b1710f2bc54232
alvarogonmar <alvarogonmar0@gmail.com>
2025-09-22 17:45:03 -0600

    Fix quick_sort_helper to ensure proper recursive calls for right partition

--- QuickSort/2-EXERCISE-Quick_Sort.py
+++ QuickSort/2-EXERCISE-Quick_Sort.py
@@ -25,6 +25,7 @@ def quick_sort_helper(my_list, left, right):
     if left < right:
         pivot_index = pivot(my_list, left, right)
         quick_sort_helper(my_list, left, pivot_index-1)  
+        quick_sort_helper(my_list, pivot_index+1, right)       
 
 def quick_sort(my_list):
     quick_sort_helper(my_list, 0, len(my_list)-1)
