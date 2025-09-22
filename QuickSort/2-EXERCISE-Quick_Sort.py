87f4c17ba553024c49ac3f2fda9ff94d3685a74c
alvarogonmar <alvarogonmar0@gmail.com>
2025-09-22 17:45:11 -0600

    Return sorted list from quick_sort_helper function

--- QuickSort/2-EXERCISE-Quick_Sort.py
+++ QuickSort/2-EXERCISE-Quick_Sort.py
@@ -26,6 +26,7 @@ def quick_sort_helper(my_list, left, right):
         pivot_index = pivot(my_list, left, right)
         quick_sort_helper(my_list, left, pivot_index-1)  
         quick_sort_helper(my_list, pivot_index+1, right)       
+    return my_list
 
 def quick_sort(my_list):
     quick_sort_helper(my_list, 0, len(my_list)-1)
