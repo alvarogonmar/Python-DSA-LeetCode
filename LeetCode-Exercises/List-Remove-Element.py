f28fbdb6760d1c6a1931dcf9bbbfc1da7f361653
alvarogonmar <alvarogonmar0@gmail.com>
2025-09-25 09:47:28 -0600

    Fix remove_element function to handle cases where the value is not found

--- LeetCode-Exercises/List-Remove-Element.py
+++ LeetCode-Exercises/List-Remove-Element.py
@@ -11,6 +11,7 @@ def remove_element(nums, val):
     while i < len(nums):
         if nums[i] == val:
             nums.pop(i)
+        else:
 
 # Test case 1: Removing a single instance of a value (1) in the middle of the list.
 nums1 = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
