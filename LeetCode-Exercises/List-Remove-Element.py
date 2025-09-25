14f970de76c7ee985f85663584959eb0023bda8d
alvarogonmar <alvarogonmar0@gmail.com>
2025-09-25 09:46:04 -0600

    Define the remove_element function in List-Remove-Element.py

--- LeetCode-Exercises/List-Remove-Element.py
+++ LeetCode-Exercises/List-Remove-Element.py
@@ -6,6 +6,7 @@
 ##########################################
 
 
+def remove_element(nums, val):
 
 # Test case 1: Removing a single instance of a value (1) in the middle of the list.
 nums1 = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
