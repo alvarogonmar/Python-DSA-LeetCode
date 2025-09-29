97b51cf978f5b35165b8f23bfbaa24f0a3586658
alvarogonmar <alvarogonmar0@gmail.com>
2025-09-29 08:21:05 -0600

    Add test case for remove_duplicates function to validate handling of empty list

--- LeetCode-Exercises/List-Remove-Duplicates.py
+++ LeetCode-Exercises/List-Remove-Duplicates.py
@@ -20,4 +20,9 @@ def remove_duplicates(nums):
             r += 1
     return l + 1
 
+# Test case 1: Empty list
+test1 = []
+print(f"Test 1 Before: {test1}")
+result1 = remove_duplicates(test1)
+
 
