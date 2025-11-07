c9dc1b4385623afcf08d98ed2f95eb4b40cdb123
alvarogonmar <alvarogonmar0@gmail.com>
2025-11-07 07:08:27 -0600

    Fix return statement in dfs_in_order method to ensure results are returned correctly

--- TreeTraversal/4-EXERCISE-DFS_In_Order.py
+++ TreeTraversal/4-EXERCISE-DFS_In_Order.py
@@ -93,7 +93,7 @@ class BinarySearchTree:
             if current_node.right is not None:
                 traverse(current_node.right)
         traverse(self.root)
-
+        return results
         
 
 
